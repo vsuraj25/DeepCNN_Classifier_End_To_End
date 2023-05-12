57f639f08628db36846dda66f8e10cef0c6953e8
Suraj Verma <stdmail2552@gmail.com>
2023-05-12 15:27:31 +0530

    Callbacks and Training config entity updated

--- src/DeepCNNClassifer/entity/config_entity.py
+++ src/DeepCNNClassifer/entity/config_entity.py
@@ -19,3 +19,22 @@ class PrepareBaseModelConfig:
     params_include_top : bool
     params_weights : str
     params_classes : int
+
+@dataclass(frozen=True)
+class PrepareCallbackConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
+
+
+@dataclass(frozen=True)
+class ModelTrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path : Path
+    training_data : Path
+    params_epoch : int
+    params_batch_size : int
+    params_is_augmented : bool
+    params_image_size : list
+
