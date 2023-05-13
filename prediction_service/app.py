2b28f09bc20267122d4b3e6fcb38dc13a525510c
Suraj Verma <stdmail2552@gmail.com>
2023-05-13 16:51:05 +0530

    app.py updated

--- prediction_service/app.py
+++ prediction_service/app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import tensorflow as tf
 import numpy as np
 
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("prediction_service/model.h5")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
